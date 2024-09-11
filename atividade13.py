a414a22515fc7e2355e4462edb9bc38127331638
marianefrra] <marianerochaferreira48@gmail.com>
2024-09-11 19:39:30 -0300

    atividade13

---
+++ atividade13.py
@@ -0,0 +1,11 @@
+# Crie um programa que receba um número do usuário e informe se ele é positivo, negativo ou zero.
+
+num = int(input("digite o número"))
+
+if num >0:
+    print("é um número positivo")
+elif num == 0:
+    print("é um número neutro")
+elif num <0: 
+    print("é um número negtivo")
+    
